1e728f169747ea30bd784f1620aee92dcc2c2a2f
Meg <70302885+meg-mir@users.noreply.github.com>
2020-10-07 14:32:11 -0500

    Added javascript to file

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
-site.js
+onsubmit 
+alert 
+<script>
+function myEventFunction() {
+  alert('You have successfully entered your submission!') ;
+}
+
+var d1 = document.getElemebtById ('submit') ;
+d1.addEventListener ('click', myEventFunction) ;
+</script>
