b548f31420d28fd3cdd8976c2096a1a241dbf7d1
Meg <70302885+meg-mir@users.noreply.github.com>
2020-10-07 19:34:06 -0500

    added variables to indicate if and else tags

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,15 @@
 function buttonPress() {
-    var submit = document.getElementByID ('submit').value ;
-    alert('You have successfully entered your submission!')
+    var first_name_field = document.getElementByID('first').value ;
+    var last_name_field = document.getElementByID('last') ;
+    var phone_field= document.getElementByID('phone').value ;
+    var email_field = document.getElementByID('email').value ;
+    var age_field = document.getElementByID('age').value ;
+    var location_field = document.getElementByID('location').value ;
+    var comments_field = document.getElementByID('comments').value ;
+    var submit_field = document.getElementByID ('submit').value ;
+    if (first && last && phone && email && age && comments) {
+      alert.button('You have successfully entered your submission!')
+    } else {
+      alert('Please fill out all fields') ;
+    }
 }
